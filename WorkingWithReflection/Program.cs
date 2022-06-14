75baa9b548ae5c0f0c47217f41b4bb82d1130ef7
AyaneSHOJI <shojiayane@gmail.com>
2022-06-14 14:06:43 +0200

    p.357 version and company updating

--- WorkingWithReflection/Program.cs
+++ WorkingWithReflection/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection; // Assembly
+using static System.Console;
+
+WriteLine("Assembly metadata:");
+Assembly? assembly = Assembly.GetEntryAssembly();  
+
+if(assembly is null)
+{
+    WriteLine("Failed to get entry assembly.");
+    return;
+}
+
+WriteLine($"   Full name: {assembly.FullName}");
+WriteLine($"   Location : {assembly.Location}");
+
+IEnumerable<Attribute> attributes = assembly.GetCustomAttributes();
+
+WriteLine($"    Assembly-level attributes:");
+foreach(Attribute attribute in attributes)
+{
+    WriteLine($"   {attribute.GetType()}");
+}
+
+AssemblyInformationalVersionAttribute? version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+WriteLine($"Version: {version?.InformationalVersion}");
+AssemblyCompanyAttribute? company = assembly.GetCustomAttribute<AssemblyCompanyAttribute>();
+WriteLine($"Company: {company?.Company}");
+
